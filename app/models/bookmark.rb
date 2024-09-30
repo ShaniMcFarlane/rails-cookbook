feb18ee530e02f9b5dde90f5ebb0928e07f83b16
shani.mcfarlane96@gmail.com <shani.mcfarlane96@gmail.com>
2024-09-30 11:46:17 +0100

    validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+  validates :recipe_id, uniqueness: { scope: :category_id }
+  # scope checks what uniqueness is against eg --> recipe id is unique against category
+  validates :recipe_id, :category_id, presence: true
+  validates :comment, length: { minimum: 6 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
   has_many :categories
+  validates :name, :description, presence: true
+  validates :name, uniqueness: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
